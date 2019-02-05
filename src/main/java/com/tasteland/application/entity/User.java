ef2a8db30309e71cfbb280393186ac5bbe732539
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-02-05 18:42:32 +0100

    User entity class

---
+++ src/main/java/com/tasteland/application/entity/User.java
@@ -0,0 +1,55 @@
+package com.tasteland.application.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user")
+@Getter @Setter @NoArgsConstructor
+public class User {
+
+
+    private Long id;
+    private String userName;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+
+    public User(String userName, String password, String firstName, String lastName, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public User(String userName, String password, String firstName, String lastName, String email,
+                Collection<Role> roles) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.roles = roles;
+    }
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", userName='" + userName + '\'' + ", password='" + "*********" + '\''
+                + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\''
+                + ", roles=" + roles + '}';
+    }
+}
