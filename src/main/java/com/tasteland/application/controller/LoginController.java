c06332baadc5c9a5fc3299544aaa602bfc6a6305
Hubert Stasiuk <hstasiuk@CORESERVICES.PL>
2019-02-07 18:07:15 +0100

    LoginController impl

---
+++ src/main/java/com/tasteland/application/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.tasteland.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showLoginForm(){
+        return "login-form";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+        return "access-denied";
+    }
+}
